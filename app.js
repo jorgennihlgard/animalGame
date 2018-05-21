6ac6c95d6660883196eca6820e09663b33eb5395
Jorgen N <j.nihlgard@outlook.com>
2018-05-21 15:47:30 +0200

    added ejs

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
 const router = express.Router();
+app.set("view engine", "ejs");
 var fs = require("fs"),
     json;
 const port = 3000;
-
 var jsonFile = require('jsonfile')
 var fileName = './db.json'
+
+
 
 jsonFile.readFile(fileName, function(err, jsonData) {
   if (err) throw err;
   
   
+  app.get("/",function(req,res){
+    res.render("index", {jsonData});
+  });
+
   for (var i = 0; i < jsonData.length; ++i) {
 
-    console.log("Emp ID: "+jsonData.users[i].id);
+    console.log("Emp ID: "+jsonData[i].id);
     console.log("Emp Name: "+jsonData[i].name);
     console.log("Emp Address: "+jsonData[i].age);
     console.log("Designation: "+jsonData[i].city);
