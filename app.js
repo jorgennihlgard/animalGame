bb2b4ddc45a0686b2887084c5f381f9a41868a96
Jorgen N <j.nihlgard@outlook.com>
2018-05-24 15:28:53 +0200

    more changes

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
-const router = express.Router();
+//const router = express.Router();
 app.set("view engine", "ejs");
-var fs = require("fs"),
-    json;
+//var fs = require("fs"),json;
 const port = 3000;
 var jsonFile = require('jsonfile')
-var fileName = './db.json'
+var dbFile = './animaldb.json'
 
+app.use(bodyParser.urlencoded({extended:true}));
 
-
-jsonFile.readFile(fileName, function(err, jsonData) {
+jsonFile.readFile(dbFile, function(err, jsonData) {
   if (err) throw err;
   
-  
   app.get("/",function(req,res){
-    res.render("index", {jsonData});
+    var answerYesNo = req.query.answer;
+    console.log(answerYesNo);
+    res.render("index", {jsonData,answerYesNo:answerYesNo});
   });
 
-  for (var i = 0; i < jsonData.length; ++i) {
+  app.post("/",function(req,res){
+   
+    console.log(req.body);
+    var answ=req.body;
+    
+    
+    jsonFile.writeFile(dbFile,answ,{flag:'a'},function(err){
+      if(err){console.log(err);}
+      
+    });
+    res.redirect("index")
+  });
 
-    console.log("Emp ID: "+jsonData[i].id);
-    console.log("Emp Name: "+jsonData[i].name);
-    console.log("Emp Address: "+jsonData[i].age);
-    console.log("Designation: "+jsonData[i].city);
+  //for (var i = 0; i < jsonData.length; ++i) {
+    
+    // console.log("Emp ID: "+jsonData[i].id);
+    // console.log("Emp Name: "+jsonData[i].name);
+    // console.log("Emp Address: "+jsonData[i].age);
+    // console.log("Designation: "+jsonData[i].city);
     console.log("----------------------------------");
-  }
+  //}
 });
 
 app.use(bodyParser.json({type: function() {return true}}));
---
+++ script.js
@@ -0,0 +1,14 @@
+alert("ddsd");
+var yesButton = document.getElementById('yesBtn');
+var counter = 0;
+yesButton.addEventListener("click", function(){
+    
+    
+        if(answerYesNo=='Yes'){
+            counter++
+            console.log(counter)
+        
+            
+    
+}
+});
